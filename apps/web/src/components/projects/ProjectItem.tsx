467f1efce457cd2687584e3ced3bc0377cfc6912
wngml317@wisenut.co.kr <wngml317@wisenut.co.kr>
2026-04-15 15:22:33 +0900

    fix: 프로젝트 API 연동

--- apps/web/src/components/projects/ProjectItem.tsx
+++ apps/web/src/components/projects/ProjectItem.tsx
@@ -8,13 +8,22 @@ import {
   ItemTitle,
 } from "../ui/item";
 
+function WarningBadge({ title, count }: { title: string; count: number }) {
+  const isWarning = count === 0;
+  return (
+    <Badge className="text-[11px]"  variant={isWarning ? "destructive" : "outline"}>
+      {title} {count}
+    </Badge>
+  );
+}
+
 export default function ProjectItem({
-  item,
-  isActive,
+  project,
+  isSelected,
   onClick,
 }: {
-  item: any;
-  isActive: boolean;
+  project: any;
+  isSelected: boolean;
   onClick: () => void;
 }) {
   return (
@@ -22,35 +31,24 @@ export default function ProjectItem({
       variant="outline"
       onClick={onClick}
       className={`w-full text-left px-3 py-2.5 rounded-lg border transition-all ${
-        isActive
-          ? 'bg-violet-50 border-violet-300 border-l-violet-500 border-l-4 '
-          : 'bg-white border-zinc-100 hover:border-violet-200 hover:bg-violet-50/50'
+        isSelected
+          ? "bg-violet-50 border-violet-300 border-l-violet-500 border-l-4 "
+          : "bg-white border-zinc-100 hover:border-violet-200 hover:bg-violet-50/50"
       }`}
-      // className={cn(
-      //   "cursor-pointer hover:bg-muted rounded-sm",
-      //   isActive && "bg-[#f5f5fd] border-[#7f77dd] border-l-4",
-      // )}
     >
-      {/* <span
-          className={`w-1.5 h-1.5 rounded-full shrink-0 ${
-            isActive ? 'bg-violet-500' : 'bg-zinc-300'
-          }`}
-        /> */}
       <ItemContent>
-        <ItemTitle className="font-medium">
-          {item.name}
-        </ItemTitle>
-        <ItemDescription className="text-xs">
-          {item.description}
-        </ItemDescription>
-        <div className="flex gap-2">
-          <Badge variant="outline">데이터셋 {item.dataset}</Badge>
-          <Badge variant="outline">시나리오 {item.scenario}</Badge>
-          <Badge variant="outline">프롬프트 {item.prompt}</Badge>
+        <ItemTitle>{project.name}</ItemTitle>
+        <ItemDescription>{project.description}</ItemDescription>
+        <div className="flex gap-1.5 pt-1">
+          <WarningBadge title="데이터셋" count={project.dataset_version_count} />
+          <WarningBadge title="시나리오" count={project.scenario_count} />
+          <WarningBadge title="프롬프트" count={project.prompt_count} />
         </div>
       </ItemContent>
-      <ItemActions>
-        <ChevronRight className={`w-3 h-3 shrink-0 ${isActive ? 'text-violet-400' : 'text-zinc-300'}`} />
+      <ItemActions >
+        <ChevronRight
+          className={`w-3 h-3 shrink-0 ${isSelected ? "text-violet-400" : "text-zinc-300"}`}
+        />
       </ItemActions>
     </Item>
   );
