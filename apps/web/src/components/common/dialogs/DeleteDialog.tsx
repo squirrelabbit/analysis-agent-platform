49bfe31614e923b15377e6940b2d4ac26810295d
wngml317@wisenut.co.kr <wngml317@wisenut.co.kr>
2026-05-13 17:33:47 +0900

    fix: 공통 컴포넌트 삭제 Dialog 수정

--- apps/web/src/components/common/dialogs/DeleteDialog.tsx
+++ apps/web/src/components/common/dialogs/DeleteDialog.tsx
@@ -9,52 +9,39 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
 import { Trash2 } from "lucide-react";
 
-export default function DeleteDialog({
-  onDelete,
-  title,
-  children,
-}: {
-  title: string,
+interface DeleteDialgProps {
+  title: string;
   onDelete: () => void;
   children?: React.ReactNode;
-}) {
+  Icon?: React.ReactNode;
+}
+
+export default function DeleteDialog({
+  title,
+  onDelete,
+  children,
+  Icon,
+}: DeleteDialgProps) {
   return (
     <Dialog>
-      <TooltipProvider>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <DialogTrigger asChild>
-              <Button
-                size="icon-xs"
-                variant="ghost"
-                className="rounded-lg hover:bg-red-50 hover:text-red-500 text-zinc-400"
-              >
-                <Trash2  />
-              </Button>
-            </DialogTrigger>
-          </TooltipTrigger>
-          <TooltipContent side="top">
-            <p className="text-xs">삭제</p>
-          </TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
+      <DialogTrigger>
+        <Button
+          variant="ghost"
+          className="hover:bg-red-50 hover:text-red-500 text-zinc-400"
+        >
+          {Icon ? Icon : <Trash2 />}
+        </Button>
+      </DialogTrigger>
       <DialogContent className="sm:max-w-sm">
         <DialogHeader>
-          <DialogTitle>{title} 삭제</DialogTitle>
+          <DialogTitle>{title}</DialogTitle>
           <DialogDescription className="text-xs">
             정말 삭제하시겠습니까? 이 작업은 되돌릴 수 없습니다.
           </DialogDescription>
           {children}
         </DialogHeader>
-
         <DialogFooter className="flex gap-2">
           <DialogClose asChild>
             <Button variant="outline">취소</Button>
